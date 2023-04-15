54e3ef573a8992bf0a08af80880d874265fd07c3
CK-Kwok87 <106216348+CK-Kwok87@users.noreply.github.com>
2023-04-15 16:28:41 +0800

    Successfully implemented dark mode.

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,40 @@
-// DARK THEME
-var icon = document.getElementById("icon");
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#icon");
 
-icon.onclick = function () {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
-    icon.src = "assets/images/dark theme icons/sun.png";
-  } else {
-    icon.src = "assets/images/dark theme icons/moon.png";
-  }
+// Check if dark mode is enabled.
+// If it's enabled, turn it off.
+// If it's disabled, turn it on.
+
+const enableDarkMode = () => {
+  // 1. Add the class darkmode to the body.
+  document.body.classList.add("darkmode");
+
+  // 2. Update darkMode in the LocalStorage
+  localStorage.setItem("darkMode", "enabled");
 };
+
+const disableDarkMode = () => {
+  // 1. Add the class darkmode to the body.
+  document.body.classList.remove("darkmode");
+
+  // 2. Update darkMode in the LocalStorage
+  localStorage.setItem("darkMode", "disabled");
+};
+
+if (darkMode == "enabled") {
+  enableDarkMode();
+  icon.src = "assets/images/dark theme icons/sun.png";
+}
+
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+    icon.src = "assets/images/dark theme icons/sun.png";
+    console.log(darkMode);
+  } else {
+    disableDarkMode();
+    icon.src = "assets/images/dark theme icons/moon.png";
+    console.log(darkMode);
+  }
+});
